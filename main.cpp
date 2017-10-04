b3142b0b54e1fede5e327bc2511262d9fb70c986
rdecker7 <rdecker7@mail.csuchico.edu>
2017-10-04 10:50:00 -0700

    Answer for quiz 2

--- main.cpp
+++ main.cpp
@@ -12,19 +12,32 @@ int main()
      do not edit this space
 
      where this comment is, the instructor will
-     add the code that gets the user's input and
-     inserts the input into every element of the
+     add the code that gets the user's input and inserts the input into every element of the
+
      matrix wordGrid
   */
-
-  //PUT YOUR CODE BELOW THIS LINE
+   
+  //PU
   //that prints each element of the matrix in the
   //following format:
   //Row _ Column _ is _
   //the first and second blanks should be replaced with the coordinates
-  //of the element's row and column and the third blank should be replaced
+  //o:wq
+  s row and column and the third blank should be replaced
   //with the value of that element
+    int max=wordGrid.size();
+   
+    for (init r=0;r<max;r++)
+    {
+      for (init c=0;c<max;index++)
+      {
+        cout<<"Row "<<r<<"Column "<<c<<"is "<<max[r][c];
+      }
+    }
 
+ return 0;
+ }
+      
   
 
 
